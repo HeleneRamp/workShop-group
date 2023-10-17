241dd2aec35c5e25f6b8e2095a12ae7080596146
Helene Rampelberg <h.rampelberg@yahoo.fr>
2023-10-17 16:47:19 +0200

    commit before merge

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,38 @@
 import "./NavBar.css"
+import PropTypes from 'prop-types'
 
-function NavBar({parrotIndex, setParrotIndex, parrotList, parrot}) {
-    
+function NavBar({ parrotIndex, setParrotIndex, parrotList, parrot }) {
+
 
     let audio = new Audio(parrot.sing)
 
     const start = () => {
         audio.play();
     }
-    
+
     const previousClick = () => {
-        setParrotIndex(parrotIndex -1)
+        setParrotIndex(parrotIndex - 1)
     }
 
     const nextClick = () => {
-        setParrotIndex(parrotIndex +1)
+        setParrotIndex(parrotIndex + 1)
     }
 
-    return ( 
+    return (
         <>
-        <div className="box">
-        <button className={parrotIndex === 0 ? "btn hidden" : "btn previous"} disabled={parrotIndex === 0} onClick={previousClick}>Previous</button>
-        <button className="btn song" onClick={start}>🎵</button>
-        <button className={parrotIndex === parrotList.length -1 ? "btn hidden" : "btn next"} disabled={parrotIndex === parrotList.length -1} onClick={nextClick}>Next</button>
-        </div>
+            <div className="box">
+                <button className={parrotIndex === 0 ? "btn hidden" : "btn previous"} disabled={parrotIndex === 0} onClick={previousClick}>Previous</button>
+                <button className="btn song" onClick={start}>🎵</button>
+                <button className={parrotIndex === parrotList.length - 1 ? "btn hidden" : "btn next"} disabled={parrotIndex === parrotList.length - 1} onClick={nextClick}>Next</button>
+            </div>
         </>
-     );
+    );
+}
+
+NavBar.propTypes = {
+    parrot: PropTypes.shape({
+        sing: PropTypes.string.isRequired,
+    }).isRequired
 }
 
 export default NavBar;
